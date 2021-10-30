df2b41b931716ac620a9da5dbece3bd192751799
aimen08 <aimen_hamza@hotmail.com>
2021-10-30 05:19:06 +0100

    fixed schema

---
+++ schemas/work.js
@@ -0,0 +1,42 @@
+export default {
+  name: "work",
+  title: "Work",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      type: "string",
+    },
+    {
+      name: "tag",
+      title: "Tag",
+      type: "string",
+    },
+    {
+      name: "desc",
+      title: "Description",
+      type: "text",
+    },
+    {
+      name: "item1",
+      title: "Framework",
+      type: "string",
+    },
+    {
+      name: "item2",
+      title: "Database",
+      type: "string",
+    },
+    {
+      name: "item3",
+      title: "Design",
+      type: "string",
+    },
+    {
+      title: "Image",
+      name: "image",
+      type: "image",
+    },
+  ],
+};
